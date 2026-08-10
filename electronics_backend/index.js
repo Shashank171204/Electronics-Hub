ce61f443b763a3eb10f02500dcbaa72f0a2b9cba
Shashank171204 <shashu1712@gmail.com>
2026-08-10 13:25:31 +0530

    added "0.0.0.0"

--- electronics_backend/index.js
+++ electronics_backend/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
-    app.listen(process.env.PORT, () => {
+    app.listen(process.env.PORT, "0.0.0.0", () => {
       console.log(`Server is running on port ${process.env.PORT}`);
     });
   })
